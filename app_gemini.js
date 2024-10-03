b21c1f55f75a5d3274c0e91049b153262a90855a
arespinoza <alfredo_espi@hotmail.com>
2024-10-03 17:34:52 -0300

    subiendo

--- app_gemini.js
+++ app_gemini.js
@@ -1,15 +1,17 @@
 import cors from 'cors'
 import express from 'express'
-import ollama from 'ollama'
 
-import { VertexAI } from "@langchain/google-vertexai-web";
+//import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
 // This helps connect to our .env file
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { OllamaEmbeddings } from "langchain/embeddings/ollama";
-import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai"
+//import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai"
+import { GoogleGenerativeAIEmbeddings }  from "@langchain/google-genai";
+
+
 import { TaskType } from "@google/generative-ai";
 
 import {TextLoader} from "langchain/document_loaders/fs/text";
@@ -17,27 +19,14 @@ import {PDFLoader} from "langchain/document_loaders/fs/pdf";
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
 import {DirectoryLoader} from "langchain/document_loaders/fs/directory";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
-
-
-import { Ollama } from "langchain/llms/ollama";
 import { loadQAStuffChain} from "langchain/chains";
 
 const app = express();
-const port = 3001;
+const port = 3000;
 app.use(cors({origin: ['http://localhost:4200','http://10.3.0.62:4200',  'http://aplicaciones.fce.unju.edu.ar', 'http://aplicaciones.unju.edu.ar']}));
 
 
 
-
-const embeddings = new OllamaEmbeddings({
-  model: "llama3.2:latest", // default value
-  baseUrl: "http://10.3.2.195:11434", // default value
-  requestOptions: {
-    useMMap: true,
-    numThread: 6,
-    numGpu: 1,
-  },
-});
 
 
 const embeddingstext = new GoogleGenerativeAIEmbeddings({
@@ -45,14 +34,6 @@ const embeddingstext = new GoogleGenerativeAIEmbeddings({
   taskType: TaskType.RETRIEVAL_DOCUMENT,
   title: "Document title",
 });
-
-const embeddingsgoogle = new GoogleGenerativeAIEmbeddings({
-  model: "gemini-1.5-flash"
-});
-
-//const documents = ["Hello World!", "Bye Bye"];
-//const documentEmbeddings = await embeddings.embedDocuments(documents);
-//console.log(documentEmbeddings);
 
 
 // Create docs with a loader
@@ -63,17 +44,15 @@ const loader = new DirectoryLoader("./docs",{
 })
 const docs = await loader.load();
 const textSplitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 1000,
-  chunkOverlap: 200,
+  chunkSize: 5000,
+  chunkOverlap: 500,
 })
 const docOutput = await textSplitter.splitDocuments(docs)
 // Load the docs into the vector store
 const vectorStore = await MemoryVectorStore.fromDocuments(
   docOutput,
-  embeddingsgoogle
+  embeddingstext
 );
-
-console.log(vectorStore);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
@@ -100,37 +79,22 @@ app.listen(port, () => {
   
       
       // Search for the most similar document
-      const resultOne = await vectorStore.similaritySearch(question, 3);
+      console.log("Resultados de la busqueda:");
+      console.log(question);
+
+      const resultOne = await vectorStore.similaritySearch(question, 5);
       console.log("Resultados de la busqueda:");
       console.log(resultOne);
 
 
 
       const geminiModel = new ChatGoogleGenerativeAI({
-        modelName: "gemini-1.5-flash",
-      });
-      const llm = new ChatGoogleGenerativeAI({
-        model: "gemini-1.5-flash",
+        modelName: "gemini-pro",
         temperature: 0,
         maxRetries: 2,
-        // apiKey: "...",
-        // other params...
       });
 
-      const llmvertex = new VertexAI({
-        model: "gemini-pro",
-        temperature: 0,
-        maxRetries: 2,
-        // other params...
-      });      
 
-
-
-      const llmA = new Ollama({ 
-        model: "llama3.2:latest",
-        baseUrl: "http://10.3.2.195:11434", // default value
-
-        });
       const chainA = loadQAStuffChain(geminiModel);
       var desde = Date.now();
       const resA = await chainA.invoke({
@@ -151,34 +115,3 @@ app.listen(port, () => {
     }
   })
 
-  /**
-  app.get('/api/ask', async (req, res) => {
-
-    var question = "";
-    try {
-  
-        if(req.query.pregunta!=null && req.query.pregunta!=""){
-        question = req.query.pregunta;
-        };
-
-        var desde = Date.now();
-        const resA = await ollama.chat({
-            model: 'gemma:latest',
-            messages: [{ role: 'user', content: question}]
-        })
-        var hasta = Date.now();
-        console.log("Segundos transcurridos de la consulta: "+String((hasta - desde) / 1000));
-        res.json({ result: resA }); // Send the response as JSON
-    }  
-      catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' }); // Send an error response
-    }
-
-
-
-
-
-})
-
- */
