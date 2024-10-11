6568a92c535f479b3cdfa24b34e22ce89c34e53b
= <=>
2024-10-11 11:34:49 -0300

    subiendo

--- app_gemini_hnsw.js
+++ app_gemini_hnsw.js
@@ -3,9 +3,6 @@ import express from 'express'
 
 //import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GoogleAIFileManager } from "@google/generative-ai/server";
-
 // This helps connect to our .env file
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -28,23 +25,6 @@ const port = 3000;
 app.use(cors({origin: ['http://localhost:4200','http://10.3.0.62:4200',  'http://aplicaciones.fce.unju.edu.ar', 'http://aplicaciones.unju.edu.ar']}));
 
 
-
-
-
-
-// Initialize GoogleGenerativeAI with your API_KEY.
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-// Initialize GoogleAIFileManager with your API_KEY.
-const fileManager = new GoogleAIFileManager(process.env.GOOGLE_API_KEY);
-// Upload the file and specify a display name.
-const uploadResponse = await fileManager.uploadFile("docs/ingresantes-ayuda.txt", {
-  mimeType: "text/plain",
-  //displayName: "Gemini 1.5 PDF",
-});
-
-
-
-
 const embeddingstext = new GoogleGenerativeAIEmbeddings({
   model: "text-embedding-004", // 768 dimensions
   taskType: TaskType.RETRIEVAL_DOCUMENT,
@@ -60,8 +40,8 @@ const loader = new DirectoryLoader("./docs",{
 })
 const docs = await loader.load();
 const textSplitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 2000,
-  chunkOverlap: 200,
+  chunkSize: 3000,
+  chunkOverlap: 300,
 })
 const docOutput = await textSplitter.splitDocuments(docs)
 // Load the docs into the vector store
@@ -86,11 +66,14 @@ app.listen(port, () => {
 
   app.get('/api/ask', async (req, res) => {
     var question = "";
+    var question_contexto = "";
     try {
 
-      var prompt="Responder en español. "
+      var prompt=`Responde primeramente presentandote como Asistente Virtual de la Facultad de Ciencias Económicas de la UNJu.
+                  Responde a la  pregunta SIEMPRE en el lenguaje español. `
       if(req.query.pregunta!=null && req.query.pregunta!=""){
-        question = prompt + req.query.pregunta;
+        question = req.query.pregunta +" ?";
+        question_contexto = prompt + req.query.pregunta + "?";
       };
   
   
@@ -98,17 +81,18 @@ app.listen(port, () => {
       // Search for the most similar document
       console.log("Pregunta:");
       console.log(question);
+      console.log(question_contexto);
 
       const resultOne = await vectorStore.similaritySearch(question, 5);
-      //console.log("Resultados de la busqueda:");
-      //console.log(resultOne);
+      console.log("Resultados de la busqueda:");
+      console.log(resultOne);
 
 
 
       const llm = new ChatGoogleGenerativeAI({
         modelName: "gemini-1.5-pro",
         temperature: 0,
-        maxRetries: 2,
+        maxRetries: 3,
       });
 
 
@@ -118,34 +102,13 @@ app.listen(port, () => {
         input_language: "Spanish",
         output_language: "Spanish",
         input_documents: resultOne,
-        question,
+        question: question_contexto,
       });
       var hasta = Date.now();
       console.log("Segundos transcurridos de la consulta: "+String((hasta - desde) / 1000));
       console.log(resA);
 
       
-
-
-
-      const model = genAI.getGenerativeModel({
-        // Choose a Gemini model.
-        model: "gemini-1.5-flash",
-      });
-      // Generate content using text and the URI reference for the uploaded file.
-      const result = await model.generateContent([
-        {
-          fileData: {
-            mimeType: uploadResponse.file.mimeType,
-            fileUri: uploadResponse.file.uri,
-          },
-        },
-        { text: "Puedo ingresar a la facultad si no termine el secundario?" },
-      ]);
-      console.log("respuesta desde fileupload");
-      console.log(result.response.text());
-      //resA = result.response.text()
-
       res.json({ result: resA }); // Send the response as JSON
   
     }  
@@ -154,4 +117,3 @@ app.listen(port, () => {
       res.status(500).json({ error: 'Internal Server Error' }); // Send an error response
     }
   })
-
